bf80e822c53dcb9d2e845cd50180478998ad473f
htjeong <lch4709@nate.com>
2019-05-29 18:33:43 +0900

    commit 19/05/29

--- src/main/java/idu/cs/controller/UserController.java
+++ src/main/java/idu/cs/controller/UserController.java
@@ -2,6 +2,7 @@ package idu.cs.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
@@ -19,41 +20,53 @@ import idu.cs.exception.ResourceNotFoundException;
 import idu.cs.repository.UserRepository;
 
 @Controller
-public class HomeController {
+public class UserController {
 	@Autowired UserRepository userRepo; // Dependency Injection
-	
-	
 	@GetMapping("/")
-	public String home(Model model) {
-		//model.addAttribute("test", "인덕 컴소");
-		//model.addAttribute("htj", "정횬타이");
+	public String home(Model model) { 
 		return "index";
 	}
-	@GetMapping("/users")//읽기 main.js에서 사용하는것
+	@GetMapping("/user-login-form")
+	public String getLoginForm(Model model) {
+		return "login";
+	}
+	@PostMapping("/login")
+	public String loginUser(@Valid User user, HttpSession session) {
+		System.out.println("login process : ");
+		User sessionUser = userRepo.findByUserId(user.getUserId());
+		if(sessionUser == null) {
+			System.out.println("id error : ");
+			return "redirect:/user-login-form";
+		}
+		if(!sessionUser.getUserPw().equals(user.getUserPw())) {
+			System.out.println("pw error : ");
+			return "redirect:/user-login-form";
+		}
+		session.setAttribute("user", sessionUser);
+		return "redirect:/";
+	}
+	@GetMapping("/user-register-form")
+	public String getRegForm(Model model) {
+		return "register";
+	}
+	@GetMapping("/users")
 	public String getAllUser(Model model) {
 		model.addAttribute("users", userRepo.findAll());
 		return "userlist";
 	}
-	@GetMapping("/user-reg-form")
-	public String getRegForm(Model model) {
-		
-		return "form";
-	}
-	@PostMapping("/users")//쓰기
-	public String createUser(@Valid @RequestBody User user, Model model) {
-		
-		userRepo.save(user);
+	@PostMapping("/users")
+	public String createUser(@Valid User user, Model model) {		
+		if(userRepo.save(user) != null)
+			System.out.println("Database 등록 성공");
+		else
+			System.out.println("Database 등록 실패");
 		model.addAttribute("users", userRepo.findAll());
 		return "redirect:/users";
 	}
-	
 	@GetMapping("/users/{id}")
 	public String getUserById(@PathVariable(value = "id") Long userId, Model model)
 			throws ResourceNotFoundException {
-		
-		//UserRepository
-		User user = userRepo.findById(userId).get();//orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
-		
+		User user = userRepo.findById(userId).get();//.orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
 		model.addAttribute("user", user);
 		return "user";
 		//return ResponseEntity.ok().body(user);
@@ -61,24 +74,23 @@ public class HomeController {
 	@GetMapping("/users/fn")
 	public String getUserByName(@Param(value="name") String name, Model model)
 			throws ResourceNotFoundException {
-		
-		//UserRepository
-		List<User> users = userRepo.findByName(name);//orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
-		
+		List<User> users = userRepo.findByName(name);//.orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
 		model.addAttribute("users", users);
 		return "userlist";
 		//return ResponseEntity.ok().body(user);
 	}
-	@PutMapping("/users/{id}")//업데이트 @patchmapping 수정한 필드만 고쳐주는것
-	public String updateUser(@PathVariable(value = "id") Long userId,@Valid User userDetails, Model model) {
-		User user = userRepo.findById(userId).get(); //user는 db로부터 읽어온 객체
-		user.setName(userDetails.getName());// userDetails는 전송한 객체
+	@PutMapping("/users/{id}") // @PatchMapping
+	public String updateUser(@PathVariable(value = "id") Long userId, 
+			@Valid User userDetails, Model model) {		
+		User user = userRepo.findById(userId).get(); 
+		// user는 DB로 부터 읽어온 객체
+		user.setName(userDetails.getName()); // userDetails는 전송한 객체
 		user.setCompany(userDetails.getCompany());
 		userRepo.save(user);
 		return "redirect:/users";
-	}	
-	@DeleteMapping("/users/{id}")//삭제
-	public String deleteUser(@PathVariable(value = "id") Long userId, Model model) {
+	}
+	@DeleteMapping("/users/{id}")
+	public String deleteUser(@PathVariable(value = "id") Long userId, Model model) {		
 		User user = userRepo.findById(userId).get();
 		userRepo.delete(user);
 		model.addAttribute("name", user.getName());
